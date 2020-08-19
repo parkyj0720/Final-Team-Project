8465c0f90ceb7d9faeacd446cdf8f8431f6730ee
sunelll <hs0647@naver.com>
2020-08-19 19:21:27 +0900

    0819/로그인화면
    희수/프론트

--- BoardCa_app/Boardca/app/src/main/java/com/example/boardca_app/ui/login/LoginViewModelFactory.java
+++ BoardCa_app/Boardca/app/src/main/java/com/example/boardca_app/ui/login/LoginViewModelFactory.java
@@ -1,8 +1,8 @@
 package com.example.boardca_app.ui.login;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.annotation.NonNull;
 
 import com.example.boardca_app.data.LoginDataSource;
 import com.example.boardca_app.data.LoginRepository;
