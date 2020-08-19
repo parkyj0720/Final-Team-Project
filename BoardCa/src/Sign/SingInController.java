f704305de53a62403c9cd3beadf251ec9545f554
Kosmo_15 <Kosmo_15@DESKTOP-K73KJF2>
2020-08-19 15:59:15 +0900

    Dto폴더 추가 및 sign컨트롤러 수정

--- BoardCa/src/Sign/SingInController.java
+++ BoardCa/src/Sign/SingInController.java
@@ -1,9 +1,21 @@
 package Sign;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 @Controller
 public class SingInController {
@@ -18,6 +30,51 @@ public class SingInController {
 		mv.setViewName("/user/signIn.jsp");
 		return mv;
 	}
+	
+	@RequestMapping()
+	public ModelAndView signPro(MemberDto dto,SessionDto sedto, HttpSession session,HttpServletRequest req)
+			throws SAXException, IOException, ParserConfigurationException {
+		
+		String ip = req.getHeader("X-FORWARDED-FOR");
+		if(ip == null) {
+			ip = req.getRemoteAddr();
+			
+			sedto.setIp(ip);
+		}
+		
+		//1.문서를 읽기위한 공장을 만들어야 한다.
+		DocumentBuilderFactory fatory = DocumentBuilderFactory.newInstance();
+		//2.빌더를 생성
+		DocumentBuilder builder = fatory.newDocumentBuilder();
+		//3.생성된 빌더를 통해서 xml문서를 Document객체로 파싱해서 가져온다.
+        //Document org에서 가져온다!!!
+		Document doc = builder.parse("http://whois.kisa.or.kr/openapi/ipascc.jsp?query=121.66.252.156&key=2020081914462601995405&answer=xml");
+		NodeList list = doc.getElementsByTagName("countryCode");
+		
+		int i = 0;
+		Element element;
+		String country = null;
+		
+		while(list.item(i) != null){
+			element = (Element) list.item(i);
+			country = element.getTextContent();
+			i++;
+		}
+		//settingDto 에 나라 코드를 넣는다.
+		sedto.setPlace(country);
+		
+		System.out.println(dto.getId());
+		
+		
+		
+		
+		
+		
+		return mv;
+	}
+	
+	
+	
 	
 	
 }
