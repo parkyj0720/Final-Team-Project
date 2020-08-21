6b2b516b2d78f40c4407278635fa11a22042351c
sunelll <hs0647@naver.com>
2020-08-21 10:45:55 +0900

    0821/복구
    commit

---
+++ BoardCa_app/Boardca/app/src/main/java/com/example/boardca_app/ui/signup/SignUpActivity.java
@@ -0,0 +1,16 @@
+package com.example.boardca_app.ui.signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.boardca_app.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
