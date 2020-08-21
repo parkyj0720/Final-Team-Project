33854491fd892c692f975b641e0ebb6db1abdbb1
sunelll <hs0647@naver.com>
2020-08-21 18:33:02 +0900

    0821
    commit

--- BoardCa_app/Boardca/app/src/main/java/com/example/boardca_app/ui/gallery/GalleryFragment.java
+++ BoardCa_app/Boardca/app/src/main/java/com/example/boardca_app/ui/gallery/GalleryFragment.java
@@ -4,12 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.boardca_app.R;
@@ -23,13 +20,15 @@ public class GalleryFragment extends Fragment {
         galleryViewModel =
                 ViewModelProviders.of(this).get(GalleryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+
+        //final TextView textView = root.findViewById(R.id.text_gallery);
+        //galleryViewModel.getText().observe(this, new Observer<String>() {
+        //    @Override
+        //    public void onChanged(@Nullable String s) {
+        //        textView.setText(s);
+        //    }
+        //});
         return root;
     }
 }
