3eb15be4abce595b6791e2eaa3fafa2a18fdd88a
sunelll <43489093+sunelll@users.noreply.github.com>
2020-08-25 23:40:56 +0900

    0825 / 희수
    app 프론트 / 중복 문제 해결

--- BoardCa_app/Boardca/app/src/main/java/com/example/boardca_app/ui/home/HomeFragment.java
+++ BoardCa_app/Boardca/app/src/main/java/com/example/boardca_app/ui/home/HomeFragment.java
@@ -16,20 +16,15 @@ import com.example.boardca_app.R;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+    ViewGroup viewGroup;
 
+    @Nullable
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+
+        viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_home, container, false);
+
+        return viewGroup;
     }
 }
