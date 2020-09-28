e3f0504d7aa5e371cea0db2b4ca8020d9ce6df05
alsrb003 <alsrb0003@naver.com>
2020-09-28 12:39:49 +0900

    app random game

--- BoardCa_app/Boardca/app/src/main/java/com/example/boardca_app/ui/home/HomeFragment.java
+++ BoardCa_app/Boardca/app/src/main/java/com/example/boardca_app/ui/home/HomeFragment.java
@@ -2,10 +2,12 @@ package com.example.boardca_app.ui.home;
 
 import android.animation.Animator;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -27,9 +29,11 @@ public class HomeFragment extends Fragment {
 
     Button btnGame;
     TextView textGame;
+    ImageView gameImage;
 
     String[] labels = {"game1", "game2", "game3", "game4", "game5", "game6", "game7", "game8", "game9", "game10"};
 
+    boolean tf = true;
 
     @Nullable
     @Override
@@ -38,9 +42,32 @@ public class HomeFragment extends Fragment {
 
         viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_home, container, false);
 
+        gameImage = (ImageView) viewGroup.findViewById(R.id.game_img);
+
         LottieAnimationView animationView = viewGroup.findViewById(R.id.animation_view);
         setUpAnimation(animationView);
-        animationView.playAnimation();
+
+        animationView.addAnimatorListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                animationView.setVisibility(View.GONE);
+                gameImage.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
 //        animationView.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View view) {
@@ -60,16 +87,48 @@ public class HomeFragment extends Fragment {
         btnGame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Random random = new Random();
-                int num = random.nextInt(10);
-                textGame.setText(labels[num]);
-                textGame.setVisibility(View.VISIBLE);
+//                Random random = new Random();
+//                int num = random.nextInt(10);
+//                textGame.setText(labels[num]);
+//                textGame.setVisibility(View.VISIBLE);
+
+                if(tf) {
+                    gameImage.setVisibility(View.GONE);
+                    animationView.setVisibility(View.VISIBLE);
+                    // 재생할 애니메이션 넣어준다.
+                    animationView.setAnimation("click_data.json");
+                    animationView.loop(true);
+                    // 반복횟수를 무한히 주고 싶을 땐 LottieDrawable.INFINITE or 원하는 횟수
+                    animationView.setRepeatCount(0);
+                    // 시작
+                    animationView.playAnimation();
+
+                    btnGame.setText("처음으로");
+
+                    tf = false;
+                } else{
+                   setUpAnimation(animationView);
+                    btnGame.setText("돌리기");
+                    tf = true;
+                }
+
+//                animationView.setAnimation("home_data.json");
+//                animationView.loop(true);
+//                // 반복횟수를 무한히 주고 싶을 땐 LottieDrawable.INFINITE or 원하는 횟수
+//                animationView.setRepeatCount(1);
+//                // 시작
+//                animationView.playAnimation();
             }
         });
         return viewGroup;
     }
 
     private void setUpAnimation(LottieAnimationView animationView) {
+        // 이미지 없애기
+        gameImage.setVisibility(View.GONE);
+        // 애니메이션 보이기
+        animationView.setVisibility(View.VISIBLE);
+
         // 재생할 애니메이션 넣어준다.
         animationView.setAnimation("home_data.json");
         animationView.loop(true);
