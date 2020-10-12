225e21312848ffceb3ff2848e75aaee3e91833b9
sunelll <hs0647@naver.com>
2020-10-12 13:03:56 +0900

    chat_link
    commit

--- BoardCa_app/Boardca/app/src/main/java/com/example/boardca_app/ui/game/GameFragment.java
+++ BoardCa_app/Boardca/app/src/main/java/com/example/boardca_app/ui/game/GameFragment.java
@@ -1,5 +1,7 @@
 package com.example.boardca_app.ui.game;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,12 +16,14 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 import com.example.boardca_app.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class GameFragment extends DialogFragment {
 
     ViewGroup viewGroup;
     WebView webView;
-
+    Uri uri;
+    private FloatingActionButton fab_chat;
 
     @Nullable
     @Override
@@ -33,10 +37,20 @@ public class GameFragment extends DialogFragment {
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebChromeClient(new WebChromeClient());
         webView.setWebViewClient(new WebViewClientClass());
-        webView.loadUrl("http://192.168.219.100:8088/BoardCa/gameMain.do"); //주소는 임시, 차후에 바꿀것.
+        webView.loadUrl("http://192.168.219.107:8088/BoardCa/gameMainApp.do"); //주소는 임시, 차후에 바꿀것.
 
-//        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://172.30.1.17:8088/BoardCa/signIn.do"));
-//        startActivity(intent);
+        fab_chat = (FloatingActionButton) viewGroup.findViewById(R.id.fab_chat);
+        fab_chat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://192.168.219.107:8088/BoardCa/index.jsp"));
+                Intent intent = new Intent(getActivity(), chatActivity.class);
+                startActivity(intent);
+                getActivity().overridePendingTransition(0, 0);
+            }
+        });
+
+
 
         return viewGroup;
     }
